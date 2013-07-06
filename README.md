a54baedeec8274949bd1d77e7509e5ead47c0a00
Dylan Pyle <me@dylanpyle.com>
2013-07-05 18:10:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Konamic Sans
+
+A [Konami Code](http://en.wikipedia.org/wiki/Konami_Code) for those of discerning taste.
+
+If triggered with the correct key sequence (↑ ↑ ↓ ↓ ← → ← → B A), it replaces whichever font you're currently using with a more cultured choice — Comic Sans MS.
---
+++ konamic-1.0.0.js
@@ -0,0 +1,33 @@
+(function(document) {
+  var index = 0,
+      konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+  var enqueue = function(key, goal, success) {
+    if(goal[index] === key){
+      if(++index === goal.length) {
+        success();
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+
+  var enhance = function() {
+    var style = document.createElement('style'),
+        rule = "body{font-family:'Comic Sans MS' !important;}";
+
+    style.type = 'text/css';
+    style.styleSheet
+      ? style.styleSheet.cssText = rule
+      : style.appendChild(document.createTextNode(rule));
+
+    document.head.appendChild(style);
+  };
+
+  document.addEventListener('keydown', function(e) {
+    var key = e.charCode || e.keyCode;
+    enqueue(key, konami, enhance);
+  });
+
+})(document);
